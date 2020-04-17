f1380888e48aea394e2a16c6d1664c150d299294
Kyle Anderson <kyle.anderson@gmail.com>
2020-04-16 23:45:53 -0400

    Revised to use __dict__

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ Date: April 16, 2020
 """
 
 import json
-import numpy as np
 import pandas as pd
 
 
@@ -33,17 +32,14 @@ class Data:
         Arguments:
             filename {str} -- self-explanatory
         """
-        dict_to_write = {
-            "df": self.df.to_json(),
-            "shape": self.shape,
-            "name": self.name,
-            "color": self.color,
-        }
+
+        dict_to_write = self.__dict__.copy()
+        dict_to_write["df"] = dict_to_write["df"].to_json()
         with open(filename, "w") as file:
             json.dump(dict_to_write, file)
 
     @classmethod
-    def from_json(cls, filename: str) -> cls:
+    def from_json(cls, filename: str) -> "Data":
         """
         from_json - Class method to create a new Data object from a json file
 
@@ -57,12 +53,10 @@ class Data:
         """
         with open(filename, "r") as file:
             dict_read = json.load(file)
-        return cls(
-            pd.read_json(dict_read["df"]),
-            dict_read["name"],
-            dict_read["color"],
-            dict_read["shape"],
-        )
+
+        dict_read["df"] = pd.read_json(dict_read["df"])
+
+        return cls(**dict_read)
 
     def __eq__(self, other):
         if (
@@ -78,30 +72,23 @@ class Data:
 
 if __name__ == "__main__":
 
-    np.random.seed(0)
-    x = np.random.randint(0, 10000, 1000)
+    import random
 
-    COLORS = ["red", "blue", "green", "yellow", "purple"]
-    SHAPES = ["square", "triangle", "circle", "line", "hexagon"]
     N = 10
-    DF = pd.DataFrame(
+    X = random.choices(range(100), k=N)
+    # pylint:disable=invalid-name
+    test_df = pd.DataFrame(
         {
             "time": range(0, N),
-            "data": np.random.randint(0, 10000, N),
-            "estimate": np.random.randint(0, 10000, N),
+            "data": random.choices(range(100), k=N),
+            "estimate": random.choices(range(100), k=N),
         }
     )
 
-    my_data = Data(DF, "Module1", "GREEN", "SQUARE")
-
-    print(my_data.df)
+    my_data = Data(test_df, "Module1", "GREEN", "SQUARE")
 
     my_data.to_json("test_data.json")
 
     my_read_data = Data.from_json("test_data.json")
-
-    print(my_read_data.df)
-
-    # IPython.embed_kernel()
 
     assert my_data == my_read_data
